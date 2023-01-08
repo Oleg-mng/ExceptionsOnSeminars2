729de2e4fc626a5b0a329f0a6c45776fe7eb2730
oleg <legik7127@gmail.com>
2023-01-08 13:00:05 +0600

    Added method to App3

--- src/main/java/App3.java
+++ src/main/java/App3.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * - Дан следующий код, исправьте его там, где требуется (задание 3)
  * - Разработайте программу, которая выбросит Exception, когда пользователь
@@ -6,14 +9,17 @@
  */
 
 public class App3 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
             int a = 90;
             int b = 3;
             System.out.println(a / b);
             printSum(23, 234);
-            int[] abc = { 1, 2 };
+            int[] abc = {1, 2};
             abc[3] = 9;
+            inputNumber();
+
+
         } catch (NullPointerException e) {
             System.out.println("Указатель не может указывать на null!");
         } catch (IndexOutOfBoundsException e) {
@@ -22,8 +28,21 @@ public class App3 {
             System.out.println("Что-то пошло не так...");
         }
     }
-    public static void printSum(Integer a, Integer b)  {
+
+    public static void printSum(Integer a, Integer b) {
         System.out.println(a + b);
+    }
+
+    public static void inputNumber() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число типа Int (не строку)");
+        try {
+            int k = in.nextInt();
+            ;
+        } catch (InputMismatchException e) {
+            System.out.println("пустые строки вводить нельзя");
+        }
+        in.close();
     }
 }
 
